099967576537e70f944145a155b703d401f64d28
Nunoo Prince Harry <tURNTABL+computer-mann@users.noreply.github.com>
2021-03-03 13:56:19 +0000

    Should have gone the other way

---
+++ Configuration/Twitter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace turbobot.Configuration
+{
+    public class Twitter
+    {
+        public Api Api { get; set; }
+        public string BearerToken { get; set; }
+    }
+
+    public class Api
+    {
+        public string Key { get; set; }
+        public string SecretKey { get; set; }
+    }
+}
---
+++ Controllers/TweetController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace turbobot.Controllers
+{
+    public class TweetController : Controller
+    {
+        private readonly HttpClient httpClient;
+
+        public TweetController(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
